90ee29e1e2992a67d7e4fe352c41c68922237260
paul_clavier <plclavier@gmail.com>
2023-04-20 00:52:19 +0200

    feat(cron): add cron weekly job

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // Launch weekly job to check if chapter is out
-// schedule(`0 ${JOB_START_HOUR} * * ${JOB_DAY}`, async () => {
-//     await scrapeChapterAvailable();
-// });
-
+import { schedule } from "node-cron";
+import { JOB_DAY, JOB_START_HOUR } from "./constants.js";
 import { scrapeAndNotifyChapterAvailable } from "./main.js";
 
-await scrapeAndNotifyChapterAvailable();
+schedule(`0 ${JOB_START_HOUR} * * ${JOB_DAY}`, async () => {
+    await scrapeAndNotifyChapterAvailable();
+});
