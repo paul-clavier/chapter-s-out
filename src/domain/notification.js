fc0aade26b8200c6aef1f783bdb7a69a31dadcc3
paul_clavier <plclavier@gmail.com>
2023-06-06 21:21:43 +0200

    fix(chapter): cast environment variable as float

--- src/domain/notification.js
+++ src/domain/notification.js
@@ -1,11 +1,11 @@
 import { sendMessageToInbox } from "../infra/notification-inbox.js";
 
-export const completeNotification = async () => {
-    console.log("Completing notification");
-    await sendMessageToInbox("Chapter is available !");
+export const completeNotification = async (chapter) => {
+    console.log(`Completing notification: chapter ${chapter}`);
+    await sendMessageToInbox(`Chapter ${chapter} is available !`);
 };
 
-export const failNotification = async () => {
-    console.log("Failing notification");
-    await sendMessageToInbox("We have not found the chapter ...");
+export const failNotification = async (chapter) => {
+    console.log(`Failing notification: chapter ${chapter}`);
+    await sendMessageToInbox(`We have not found the chapter ${chapter} ...`);
 };
--- src/main.js
+++ src/main.js
@@ -6,26 +6,26 @@ import {
 import { fetchChapterAvailable } from "./domain/scraping.js";
 
 let tryNumber = 0;
-let currentMangaChapter = process.env.CHAPTER;
+let currentMangaChapter = parseFloat(process.env.CHAPTER);
 
 export const scrapeAndNotifyChapterAvailable = async () => {
     console.log(`Starting job for chapter ${currentMangaChapter}`);
     const isChapterAvailable = await fetchChapterAvailable(currentMangaChapter);
 
     if (isChapterAvailable) {
-        completeNotification();
+        completeNotification(currentMangaChapter);
         tryNumber = 0;
         currentMangaChapter += 1;
         return;
     }
 
     if (tryNumber > JOB_LIMIT) {
-        await failNotification();
+        await failNotification(currentMangaChapter);
         return;
     }
 
     if (tryNumber % 100 === 0) {
-        console.log(`Try number ${tryNumber}`)
+        console.log(`Try number ${tryNumber}`);
     }
     tryNumber += 1;
     await setTimeout(scrapeAndNotifyChapterAvailable, JOB_REFRESH_RATE);
